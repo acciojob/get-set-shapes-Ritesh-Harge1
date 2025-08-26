c99b1b5ba15e73b5fe29e9e38b8f0dce9950bb75
RITESH HARGE <135429163+Ritesh-Harge1@users.noreply.github.com>
2025-08-26 11:11:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,47 @@
-//complete this code
-class Rectangle {}
+// script.js
 
-class Square extends Animal {}
+class Rectangle {
+  constructor(width, height) {
+    this._width = width;
+    this._height = height;
+  }
 
-// Do not change the code below this line
-window.Rectangle = Rectangle;
-window.Square = Square;
+  // getter for width
+  get width() {
+    return this._width;
+  }
+
+  // getter for height
+  get height() {
+    return this._height;
+  }
+
+  // method to calculate area
+  getArea() {
+    return this._width * this._height;
+  }
+}
+
+class Square extends Rectangle {
+  constructor(side) {
+    super(side, side); // call Rectangle constructor with equal width & height
+    this._side = side;
+  }
+
+  // method to calculate perimeter of square
+  getPerimeter() {
+    return this._side * 4;
+  }
+}
+
+// Export to browser (Cypress will pick up from window)
+if (typeof window !== "undefined") {
+  window.Rectangle = Rectangle;
+  window.Square = Square;
+}
+
+// Export to Node.js/CommonJS if needed
+if (typeof module !== "undefined" && module.exports) {
+  module.exports = { Rectangle, Square };
+}
+
